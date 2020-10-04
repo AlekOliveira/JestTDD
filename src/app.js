485d16bcc4e7cf38205df2b7cd22d5bd69ea2043
Alexandre <alexandre.cardozo1@gmail.com>
2020-10-04 18:00:23 -0300

    initial configs

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 const express = requires('express');
 
-
 class AppController {
   constructor( ){
     this.express = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.express.use(express.json());
+  }
+
+  routes() {
+    this.express.use(require('./routes'));
   }
 }
+
+module.exports = new AppController().express;
